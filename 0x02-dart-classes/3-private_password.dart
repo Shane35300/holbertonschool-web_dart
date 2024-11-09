877c0f293bfe87896a4c303bc2922dbaad877f62
shane <6874@holbertonstudents.com>
2024-11-09 11:21:49 +0100

    task3

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,33 @@
+class Password {
+  String _password;
+
+  Password({required String password}) : _password = password;
+
+  bool isValid() {
+    bool isANumber = false;
+    bool isInUppercase = false;
+    bool isInLowercase = false;
+
+    if (_password.length >= 8 && _password.length <= 16) {
+      for (int i = 0; i < _password.length; i++) {
+        if (_password.codeUnitAt(i) >= 48 && _password.codeUnitAt(i) < 58) {
+          isANumber = true;
+        } else if (_password.codeUnitAt(i) > 64 &&
+            _password.codeUnitAt(i) < 91) {
+          isInUppercase = true;
+        } else if (_password.codeUnitAt(i) > 96 &&
+            _password.codeUnitAt(i) < 123) {
+          isInLowercase = true;
+        }
+      }
+      if (isANumber && isInLowercase && isInUppercase) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  String toString() {
+    return 'Your Password is: $_password';
+  }
+}
