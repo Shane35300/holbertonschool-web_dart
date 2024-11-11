b00c7b7063f7eeb854e4c4a2bbd81efbdb6a5ff5
shane <6874@holbertonstudents.com>
2024-11-11 14:39:32 +0100

    task 4

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,22 @@
+import '4-util.dart';
+import 'dart:convert';
+
+Future<double> calculateTotal() async {
+  try {
+    double total = 0.0;
+
+    String userData = await fetchUserData();
+    Map<String, dynamic> object = jsonDecode(userData);
+    String orders = await fetchUserOrders(object['id']);
+    List<dynamic> listOfProducts = jsonDecode(orders);
+    for (String product in listOfProducts) {
+      String priceStr = await fetchProductPrice(product);
+      double price = jsonDecode(priceStr) + 0.0;
+      total += price;
+    }
+    return total;
+  } catch (error) {
+    print(error);
+    return -1;
+  }
+}
